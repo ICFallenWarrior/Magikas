0768e6241a8be3112e2f76c0ec5e17860159a873
Tiago Sousa <tmsousa98@gmail.com>
2022-03-27 23:00:14 +0100

    Magikas Prof Tutorials
    The project working with every tutorial the professsor has made.

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/playersRoutes');
+var roomsRouter = require('./routes/roomsRoutes');
 
 var app = express();
+var cardsRouter = require('./routes/cardsRoutes');
+var cookieParser = require('cookie-parser');
 
+
+            
+app.use(cookieParser('VERY SECRET SECRET')); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/rooms', roomsRouter);
+app.use('/api/cards',cardsRouter);
 
 module.exports = app;
---
+++ public/javascripts/gamelib.js
@@ -0,0 +1,22 @@
+const width = 1000;
+const height = 400;
+
+var boardMan;
+
+function preload() {
+    BoardManager.preloadImages();
+    boardMan = new BoardManager(width,height,0,0,room);
+    boardMan.initBoard();
+}
+
+function setup() {
+    var canvas = createCanvas(width, height);
+    canvas.parent('game');
+}
+function draw() {
+    background(220);
+    boardMan.draw();
+}
+function mouseClicked() {
+    boardMan.click(mouseX,mouseY);     
+}
