b30b553d5f52e84d92482cdb5dc341875ab6014e
Tiago Sousa <tmsousa98@gmail.com>
2022-04-21 15:31:45 +0100

    Update cardsModel.js

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -2,7 +2,7 @@ var pool = require('./connection.js')
 
 module.exports.getAllCards = async function () {
     try {
-      let sql = `Select * from card`;
+      let sql = `Select * from card order by random() limit 5`;
       let result = await pool.query(sql);
       let cards = result.rows;
       return { status: 200, result: cards };
