0db1aa4d9f0e0a3ffba557efa4ed4b2ba7a8e2c2
Edmundo Marvão <shiripiti@hotmail.com>
2022-05-16 00:34:13 +0100

    Rework

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/playersRoutes');
-var roomsRouter = require('./routes/roomsRoutes');
+var playersRouter = require('./routes/playersRoutes');
+var cardsRouter = require('./routes/cardsRoutes');
 
 var app = express();
-var cardsRouter = require('./routes/cardsRoutes');
-var cookieParser = require('cookie-parser');
 
-
-            
-app.use(cookieParser('VERY SECRET SECRET')); 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/rooms', roomsRouter);
-app.use('/api/cards',cardsRouter);
+app.use('/api/players', playersRouter);
+app.use('/api/cards', cardsRouter);
 
 module.exports = app;
--- database/Magikas_Insert.sql
+++ database/Magikas_Insert.sql
@@ -1,6 +1,5 @@
-
 insert into card (crd_name, crd_description) values('Dark','Dark consumes Forst and Water');
-insert into card (crd_name, crd_description) values('Ligth','Ligth destroys Dark and Fire');
+insert into card (crd_name, crd_description) values('Light','Light destroys Dark and Fire');
 insert into card (crd_name, crd_description) values('Forest','Forest absovers Ligth and Water');
 insert into card (crd_name, crd_description) values('Water','Water beats Fire and eats Ligth');
 insert into card (crd_name, crd_description) values('Fire','Fire burns Forest and lights Dark');
@@ -22,8 +21,41 @@ insert into cardwcard (cwc_cwins_id, cwc_clooses_id) values(2,5);
 insert into cardwcard (cwc_cwins_id, cwc_clooses_id) values(5,1);
 insert into cardwcard (cwc_cwins_id, cwc_clooses_id) values(1,3);
 
-insert into room (roo_name, roo_topcard_id) values('Room 1',1);
+insert into match (mt_turn, mt_finished)
+values (0, false);
 
-insert into player (ply_name,ply_passwd) values ('John','Doe');
+insert into pmstate (pms_name)
+values ('PlayCard');
+insert into pmstate (pms_name)
+values ('Attack');
+insert into pmstate (pms_name)
+values ('Endturn');
+insert into pmstate (pms_name)
+values ('Wait');
+
+insert into playermatch (pm_player_id, pm_match_id, pm_state_id, pm_hp)
+values (1, 1, 1, 3);
+insert into playermatch (pm_player_id, pm_match_id, pm_state_id, pm_hp)
+values (2, 1, 4, 3);
+
+insert into cardpos (cp_name)
+values ('Hand');
+insert into cardpos (cp_name)
+values ('Table');
+insert into cardpos (cp_name)
+values ('TablePlayed');
+
+insert into deck (deck_pm_id, deck_pos_id, deck_card_id, deck_card_hp)
+values (1, 1, 1, 4);
+insert into deck (deck_pm_id, deck_pos_id, deck_card_id, deck_card_hp)
+values (1, 1, 3, 4);
+insert into deck (deck_pm_id, deck_pos_id, deck_card_id, deck_card_hp)
+values (1, 1, 1, 4);
+insert into deck (deck_pm_id, deck_pos_id, deck_card_id, deck_card_hp)
+values (2, 1, 2, 4);
+insert into deck (deck_pm_id, deck_pos_id, deck_card_id, deck_card_hp)
+values (2, 1, 5, 4);
+insert into deck (deck_pm_id, deck_pos_id, deck_card_id, deck_card_hp)
+values (2, 1, 3, 4);
 
 commit;
